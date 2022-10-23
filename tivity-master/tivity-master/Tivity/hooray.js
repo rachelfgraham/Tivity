1986c3d0049290308b021024b73ba849a9910607
rachelfgraham <81714517+rachelfgraham@users.noreply.github.com>
2022-10-23 10:20:05 -0700

    Add files via upload

---
+++ tivity-master/tivity-master/Tivity/hooray.js
@@ -0,0 +1,7 @@
+document.getElementById("button").addEventListener('click', onClick);
+
+function onClick() {  
+  document.getElementById("body").style.width = "500px";
+  document.getElementById("body").style.height = "500px";
+  document.getElementById("hooray").style.display = "block";
+}
